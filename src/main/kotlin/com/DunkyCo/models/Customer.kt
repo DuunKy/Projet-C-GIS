242f0fd6347e6c72bef5bed653c4cfbc6c8b86f5
Liliane Dabo <lilianedabo@outlook.com>
2023-12-11 19:26:17 +0100

    Skeleton of better HTTP native C# API

--- src/main/kotlin/com/DunkyCo/models/Customer.kt
+++
@@ -1,8 +0,0 @@
-package com.DunkyCo.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class e_User(val uid: String, val username: String, val password: String) // Defining of classes according to database
-val userStorage = mutableListOf<e_User>() // In-memory storage
-
